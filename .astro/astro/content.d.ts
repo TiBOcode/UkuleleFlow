176522d83b3d7ce0b57c8b1b4423a14eb2c8a753
TiBOcode <thibaut.gb@gmail.com>
2024-12-23 17:20:02 +0100

    VS Risky Push

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -275,5 +275,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../../src/content/config.js");
+	export type ContentConfig = typeof import("./../../src/content/config.js");
 }
